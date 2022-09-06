469b5724b6fed5e5ce337b65db6272b6cb9102f8
fjp321 <45053792+fjp321@users.noreply.github.com>
2022-09-06 16:39:23 -0400

    Update install_deb.sh

--- install_deb.sh
+++ install_deb.sh
@@ -1,1 +1,5 @@
-sudo apt install gimp freecad 0ad gnuradio iverilog gtkwave octave r-base python3-pip xournalpp vim gparted
+sudo apt install gimp freecad 0ad gnuradio iverilog gtkwave octave r-base python3-pip xournalpp vim gparted godot3
+wget https://raw.github.com/ryanoasis/nerd-fonts/master/install.sh
+sudo chmod u+x install.sh
+./install.sh inconsolata
+rm install.sh
